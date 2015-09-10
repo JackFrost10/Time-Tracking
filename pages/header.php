c5474aa1b70f0157ec9f875b1abf7259e370c72a
CuppaCoffee <lyndon.i0911@gmail.com>
2015-09-10 23:16:48 +0800

    update 1.2 - hot-fix
    fixed some links in the UI path.
    - cleaned directory
    - UI path hotfix

--- pages/header.php
+++ pages/header.php
@@ -52,7 +52,7 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="index.html">Admin Dash</a>
+                <a class="navbar-brand" href="index.php">Admin Dash</a>
             </div>
             <!-- /.navbar-header -->
 
